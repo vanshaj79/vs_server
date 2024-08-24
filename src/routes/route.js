832d10bc1d133d3202d89df105d3c2e6338e7364
vanshaj <vanshajgangwar88@gmail.com>
2024-08-24 21:13:54 +0530

    initial commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,7 +8,11 @@ import {
 } from "../controllers/shop.controller.js";
 
 const router = Router();
-
+router.get('/',(req,res)=>{
+      return res.status(200).json({
+        server:"server is running"
+      })
+})
 router.get("/shopifyCustomers", shopifyCustomers);
 router.get("/shopifyProducts", shopifyProducts);
 router.post("/shopifyOrders", shopifyOrders);
