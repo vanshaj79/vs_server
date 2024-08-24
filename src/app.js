18d4cdc6b5642bf3685f9890ff2a1f5e920452cd
vanshaj <vanshajgangwar88@gmail.com>
2024-08-24 21:57:20 +0530

    eror fixes

--- src/app.js
+++ src/app.js
@@ -11,5 +11,10 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }));
 
 app.use('/api',appRoute)
+app.get('/',(req,res)=>{
+    return res.status(200).json({
+      server:"server is running"
+    })
+})
 
 export { app }
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,11 +8,6 @@ import {
 } from "../controllers/shop.controller.js";
 
 const router = Router();
-router.get('/',(req,res)=>{
-      return res.status(200).json({
-        server:"server is running"
-      })
-})
 router.get("/shopifyCustomers", shopifyCustomers);
 router.get("/shopifyProducts", shopifyProducts);
 router.post("/shopifyOrders", shopifyOrders);
